6643d9806d95862bcebe7b250c0f0886d3fcd4b0
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-12 02:45:05 +0530

    did some changes in student listing

--- src/app/pages/students-list/students-list.ts
+++ src/app/pages/students-list/students-list.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { log } from 'console';
+// import { log } from 'console';.
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
@@ -17,7 +17,10 @@ export class StudentsList implements OnInit {
   // 1. Inject the HttpClient
   private http = inject(HttpClient);
   private cdr = inject(ChangeDetectorRef);
-
+  successMessage: any;
+  showDeleteModal: boolean = false;
+  selectedStudentId: number | null = null;
+  selectedStudentName:any
     constructor(
     private router: Router,
     private activateRoute: ActivatedRoute,
@@ -33,7 +36,7 @@ export class StudentsList implements OnInit {
     this.http.get<any[]>(url).subscribe({
       next: (data) => {
         this.students = data;
-        console.log(this.students, 'this.students');
+        // console.log(this.students, 'this.students');
         this.cdr.detectChanges();
         
       },
@@ -44,5 +47,40 @@ export class StudentsList implements OnInit {
   }
   detialsRedirecction(id: any){
    this.router.navigate(['/student-detials'], { queryParams: { id: id }});
+  }
+
+  deleteStudent(id: number) {
+    let url = `http://127.0.0.1:8000/students_delete/${id}`;
+    this.http.delete<any>(url).subscribe({
+      next: (data) => {
+        this.successMessage = typeof data === 'string' ? data : (data?.message || 'Student deleted successfully!');
+        this.getContactInfo(); // Refresh your listing table
+
+        // Hide success banner automatically after 4 seconds
+        setTimeout(() => {
+          this.successMessage = '';
+        }, 2000);
+      },
+      error: (error) => {
+        console.error("There was an error!", error);
+      }
+    });
+  }
+
+openDeleteModal(data: any) {
+    this.selectedStudentId = data.id;
+    this.selectedStudentName = data.name;
+    this.showDeleteModal = true;
+  }
+// Close confirmation modal
+  closeDeleteModal() {
+    this.showDeleteModal = false;
+    this.selectedStudentId = null;
+  }
+  confirmDelete() {
+    if (this.selectedStudentId !== null) {
+      this.deleteStudent(this.selectedStudentId);
+      this.closeDeleteModal();
+    }
   }
 }
