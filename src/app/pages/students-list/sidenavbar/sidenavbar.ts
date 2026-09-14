d0cbb0bbe92fc1a254a6e8ecf8e0e37221630429
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-14 19:09:03 +0530

    sidebar changes

--- src/app/pages/students-list/sidenavbar/sidenavbar.ts
+++ src/app/pages/students-list/sidenavbar/sidenavbar.ts
@@ -25,7 +25,7 @@ export class Sidenavbar {
     this.router.navigate(['/add-student']);
   }
   navigateToDashboard() {
-    // debugger;
+    debugger;
     console.log("navigateToDashboard");
     this.router.navigate(['/  ']);
   }
